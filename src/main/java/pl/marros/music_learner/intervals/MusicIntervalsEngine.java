50db2590459777453d5be1a32227b949af337646
marrosuksw <marcelrossi03@gmail.com>
2026-09-13 20:05:29 +0200

    misc: adjusted MAX_SEMITONE_SHIFT values, removed unnecessary space between lines

--- src/main/java/pl/marros/music_learner/intervals/MusicIntervalsEngine.java
+++ src/main/java/pl/marros/music_learner/intervals/MusicIntervalsEngine.java
@@ -1,10 +1,7 @@
 package pl.marros.music_learner.intervals;
 
-
 import lombok.NoArgsConstructor;
 import pl.marros.music_learner.notes.MusicNotes;
-
-
 /*
 * Responsible for:
 *   - Calculating intervals -> fetch Perfect Fifth based on a given note and interval with getIntervalNote
@@ -13,26 +10,22 @@ import pl.marros.music_learner.notes.MusicNotes;
 * */
 @NoArgsConstructor
 public class MusicIntervalsEngine {
-
-    private static final int MAX_SEMITONE_SHIFT_RIGHT = 13;
-    private static final int MAX_SEMITONE_SHIFT_LEFT = -13;
-
+    // TODO: reinvestigate if the semitone limit is needed - it could be unlimited, with modulo operations the notes would behave like in a circle
+    private static final int MAX_SEMITONE_SHIFT_RIGHT = 12;
+    private static final int MAX_SEMITONE_SHIFT_LEFT = -12;
 
     public MusicNotes shiftSemitones(MusicNotes note, int shift){
         if (shift < MAX_SEMITONE_SHIFT_LEFT || shift > MAX_SEMITONE_SHIFT_RIGHT)
-            throw new IllegalArgumentException("Unexpected shift arg: Note shift cannot be greater than 11 and lower than -11. Provided: " + shift);
+            throw new IllegalArgumentException("Unexpected shift arg: Note shift cannot be greater than 12 and lower than -12. Provided: " + shift);
         final var moduloAmount = 12;
-        MusicNotes musicNotes = MusicNotes.findByName(note.getName()); // note provided by user
-        var shiftedNoteOrder = (moduloAmount + shift + musicNotes.getOrder()) % 12;
+        var musicNotes = MusicNotes.findByName(note.getName()); // note provided by user
+        var shiftedNoteOrder = (moduloAmount + shift + musicNotes.getOrder()) % 12; // shifted note order, where order of C = 0, D = 1, E = 2, etc.
         return MusicNotes.findByOrder(shiftedNoteOrder);
     }
-
     public MusicNotes getIntervalNote(MusicNotes note, MusicIntervals interval) {
         if(interval==null){
             throw new IllegalArgumentException("Interval cannot be null for MusicTheoryEngine::getIntervalNote()");
         }
         return shiftSemitones(note, interval.getSemitones());
     }
-    // Think about implementing getters for each interval: might be boilerplate as the method is constructed in such a way that each call will be descriptive
-
 }
