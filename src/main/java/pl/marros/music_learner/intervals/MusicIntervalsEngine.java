8db800d0812b8d384344a20dfc3ce14efcd5c17f
marrosuksw <marcelrossi03@gmail.com>
2026-09-15 22:24:37 +0200

    feat: removed the max semitone shift validation and added abs function in note shift equation

--- src/main/java/pl/marros/music_learner/intervals/MusicIntervalsEngine.java
+++ src/main/java/pl/marros/music_learner/intervals/MusicIntervalsEngine.java
@@ -10,16 +10,12 @@ import pl.marros.music_learner.notes.MusicNotes;
 * */
 @NoArgsConstructor
 public class MusicIntervalsEngine {
-    // TODO: reinvestigate if the semitone limit is needed - it could be unlimited, with modulo operations the notes would behave like in a circle
-    private static final int MAX_SEMITONE_SHIFT_RIGHT = 12;
-    private static final int MAX_SEMITONE_SHIFT_LEFT = -12;
 
     public MusicNotes shiftSemitones(MusicNotes note, int shift){
-        if (shift < MAX_SEMITONE_SHIFT_LEFT || shift > MAX_SEMITONE_SHIFT_RIGHT)
-            throw new IllegalArgumentException("Unexpected shift arg: Note shift cannot be greater than 12 and lower than -12. Provided: " + shift);
+
         final var moduloAmount = 12;
-        var musicNotes = MusicNotes.findByName(note.getName()); // note provided by user
-        var shiftedNoteOrder = (moduloAmount + shift + musicNotes.getOrder()) % 12; // shifted note order, where order of C = 0, D = 1, E = 2, etc.
+        var musicNotes = MusicNotes.findByName(note.getName()); // note provided by the user
+        var shiftedNoteOrder = Math.abs(moduloAmount + shift + musicNotes.getOrder()) % moduloAmount; // shifted note order, where order of C = 0, D = 1, E = 2, etc.
         return MusicNotes.findByOrder(shiftedNoteOrder);
     }
     public MusicNotes getIntervalNote(MusicNotes note, MusicIntervals interval) {
